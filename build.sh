cf54411dc60d9cc874ca70669dafe85ac5855373
Simon Howard <fraggle@soulsphere.org>
2024-03-03 12:39:30 -0500

    Add some checks to build script.

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+
+set -eu -o pipefail
+
 rm -f newdoom1.wad
+
+if ! deutex -version >/dev/null; then
+    echo "deutex not installed."
+    exit 1
+fi
+
 deutex -doom2 bootstrap/ -iwad -build wadinfo.txt newdoom1.wad
-wadptr -q -c newdoom1.wad
+
+if wadptr -version >/dev/null; then
+    wadptr -q -c newdoom1.wad
+else
+    echo "wadptr not installed; can't compress WAD."
+fi
