c9b97153fee889037778405eb9c96632ed2041fa
Simon Howard <fraggle@soulsphere.org>
2024-03-14 19:14:34 -0400

    Add build of "silent" IWAD.
    A lot of "it runs Doom" hacks get Doom running on devices that don't
    even produce any sound, so they would be able to make use of an even
    smaller IWAD file.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -eu -o pipefail
 
-rm -f newdoom1.wad
+rm -f newdoom1.wad newdoom1_silent.wad silent.wad
 
 if ! deutex -version >/dev/null; then
     echo "deutex not installed."
@@ -11,8 +11,14 @@ fi
 
 deutex -doom2 bootstrap/ -iwad -build wadinfo.txt newdoom1.wad
 
+deutex -doom2 bootstrap/ -build silent.txt silent.wad
+cp newdoom1.wad newdoom1_silent.wad
+# TODO: We should strip the _DEUTEX_ lump that deutex leaves behind.
+deutex -doom2 bootstrap/ -join newdoom1_silent.wad silent.wad
+
 if wadptr -version >/dev/null; then
     wadptr -wipesides -q -c newdoom1.wad
+    wadptr -wipesides -q -c newdoom1_silent.wad
 else
     echo "wadptr not installed; can't compress WAD."
 fi
