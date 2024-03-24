d4c149d619d63ab825bbc6c21dd98e00832d769f
Simon Howard <fraggle@soulsphere.org>
2024-03-24 13:59:31 -0400

    First step in "inverting" the build system.
    Until now, the build has worked by building newdoom1.wad and then
    merging in other WADs to "remove" resources not needed for that
    version (eg. empty sound effects and music tracks for the silent
    version). Instead, the new build system will work by adding instead
    of subtracting; we will start with a small "kernel" WAD and
    progressively adding to it. This results in configuration files that
    are clearer since they add the things that are relevant to that WAD,
    rather than the opposite.

--- build.sh
+++ build.sh
@@ -2,27 +2,37 @@
 
 set -eu -o pipefail
 
-rm -f newdoom1.wad newdoom1_silent.wad newdoom1_1lev.wad \
-      silent.wad 1lev.wad
+rm -f kernel.wad \
+      newdoom1_1lev_nosound.wad 1lev.wad \
+      newdoom1_1lev.wad 1lev_sounds.wad \
+      newdoom1_silent.wad silent.wad \
+      newdoom1.wad
 
 if ! deutex -version >/dev/null; then
     echo "deutex not installed."
     exit 1
 fi
 
+deutex -doom2 bootstrap/ -iwad -build kernel.txt kernel.wad
+
+# E1M1-only WAD
+cp kernel.wad newdoom1_1lev_nosound.wad
+deutex -doom2 bootstrap/ -build 1lev.txt 1lev.wad
+deutex -doom2 bootstrap/ -join newdoom1_1lev_nosound.wad 1lev.wad
+
+cp newdoom1_1lev_nosound.wad newdoom1_1lev.wad
+deutex -doom2 bootstrap/ -build 1lev_sounds.txt 1lev_sounds.wad
+deutex -doom2 bootstrap/ -join newdoom1_1lev.wad 1lev_sounds.wad
+
+# IWAD
 deutex -doom2 bootstrap/ -iwad -build wadinfo.txt newdoom1.wad
 
 # Silent WAD
 deutex -doom2 bootstrap/ -build silent.txt silent.wad
 cp newdoom1.wad newdoom1_silent.wad
-# TODO: We should strip the _DEUTEX_ lump that deutex leaves behind.
 deutex -doom2 bootstrap/ -join newdoom1_silent.wad silent.wad
 
-# E1M1-only WAD
-deutex -doom2 bootstrap/ -build 1lev.txt 1lev.wad
-cp newdoom1.wad newdoom1_1lev.wad
 # TODO: We should strip the _DEUTEX_ lump that deutex leaves behind.
-deutex -doom2 bootstrap/ -join newdoom1_1lev.wad 1lev.wad
 
 if wadptr -version >/dev/null; then
     wadptr -wipesides -q -c newdoom1.wad
